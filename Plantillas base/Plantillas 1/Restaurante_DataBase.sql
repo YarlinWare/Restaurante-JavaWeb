1c664f7fa3fc43b318c0624a88a89faef179748d
Cyara <cmyarap@correo.uditrital.edu.co>
2020-03-19 15:57:18 -0500

    Se organizan archivos

--- Plantillas base/Plantillas 1/Restaurante_DataBase.sql
+++ Plantillas base/Plantillas 1/Restaurante_DataBase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-03-2020 a las 23:06:55
+-- Tiempo de generación: 19-03-2020 a las 21:11:51
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.2
 
@@ -21,6 +21,33 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `restaurante_database`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `idCategoria` int(11) NOT NULL,
+  `nombres` varchar(30) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `descripcion` varchar(30) COLLATE utf8_spanish2_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`idCategoria`, `nombres`, `descripcion`) VALUES
+(1, 'Entradas', NULL),
+(2, 'Ensaladas/Bowls', NULL),
+(3, 'Veggie', NULL),
+(4, 'Pastas', NULL),
+(5, 'Pizzas', NULL),
+(6, 'Panini', NULL),
+(7, 'Horno', NULL),
+(8, 'Bebidas', NULL),
+(9, 'Postres', NULL);
 
 -- --------------------------------------------------------
 
@@ -136,21 +163,22 @@ CREATE TABLE `productos` (
   `nombre` varchar(30) COLLATE utf8_spanish2_ci DEFAULT NULL,
   `descripcion` text COLLATE utf8_spanish2_ci DEFAULT NULL,
   `valor` int(30) DEFAULT NULL,
-  `estado` tinyint(1) DEFAULT NULL
+  `estado` tinyint(1) DEFAULT NULL,
+  `idCategoria` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`idProducto`, `nombre`, `descripcion`, `valor`, `estado`) VALUES
-(1, 'Panino Caprese', 'Queso bocconcini de búfala, mozzarella, albahaca y tomates secos, con pesto y reducción de balsámico. ', 19500, 1),
-(2, 'Bowl Caprese', 'Queso bocconcini de búfala, pasta penne a la napolitana, albahaca y tomates cherry, con aceite de oliva. ', 21000, 1),
-(3, 'Bowl Feta', 'Queso bocconcini de búfala o tofu, pasta penne al pesto, maíz, queso feta, ajonjolí, tomates cherry, aguacate y mix de lechugas; con vinagreta campiña.', 20000, 1),
-(4, 'Crema de champiñones', 'Crema de champiñones acompañada con pancitos y parmesano. ', 13500, 1),
-(5, 'Panino Pollo Carbonara', 'Pollo a la plancha con champiñón portobello y tocineta; cubierto con salsa carbonara y mozzarella. ', 26100, 1),
-(6, 'Canelones Ricotta y Espinaca', 'Tres rollos de pasta artesanal, rellenos con espinaca, ricotta y tomates secos, gratinados con salsa blanca, napolitana y parmesano. ', 21600, 1),
-(7, 'Bowl Pesto', 'Queso bocconcini de búfala o tofu, cuscús al pesto, tomates cherry, champiñones salteados, zanahorias rostizadas y mix de lechugas; con aderezo ranch. ', 21000, 1);
+INSERT INTO `productos` (`idProducto`, `nombre`, `descripcion`, `valor`, `estado`, `idCategoria`) VALUES
+(1, 'Panino Caprese', 'Queso bocconcini de búfala, mozzarella, albahaca y tomates secos, con pesto y reducción de balsámico. ', 19500, 1, 0),
+(2, 'Bowl Caprese', 'Queso bocconcini de búfala, pasta penne a la napolitana, albahaca y tomates cherry, con aceite de oliva. ', 21000, 1, 0),
+(3, 'Bowl Feta', 'Queso bocconcini de búfala o tofu, pasta penne al pesto, maíz, queso feta, ajonjolí, tomates cherry, aguacate y mix de lechugas; con vinagreta campiña.', 20000, 1, 0),
+(4, 'Crema de champiñones', 'Crema de champiñones acompañada con pancitos y parmesano. ', 13500, 1, 0),
+(5, 'Panino Pollo Carbonara', 'Pollo a la plancha con champiñón portobello y tocineta; cubierto con salsa carbonara y mozzarella. ', 26100, 1, 0),
+(6, 'Canelones Ricotta y Espinaca', 'Tres rollos de pasta artesanal, rellenos con espinaca, ricotta y tomates secos, gratinados con salsa blanca, napolitana y parmesano. ', 21600, 1, 0),
+(7, 'Bowl Pesto', 'Queso bocconcini de búfala o tofu, cuscús al pesto, tomates cherry, champiñones salteados, zanahorias rostizadas y mix de lechugas; con aderezo ranch. ', 21000, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -184,6 +212,13 @@ INSERT INTO `usuario` (`idUsuario`, `idPerfil`, `nombres`, `apellidos`, `correo`
 --
 
 --
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`idCategoria`),
+  ADD KEY `idCategoria` (`idCategoria`);
+
+--
 -- Indices de la tabla `factura`
 --
 ALTER TABLE `factura`
@@ -202,6 +237,86 @@ ALTER TABLE `formapago`
 ALTER TABLE `pedido`
   ADD PRIMARY KEY (`idPedido`),
   ADD KEY `idUsuario` (`idUsuario`);
+
+--
+-- Indices de la tabla `pedido_detalle`
+--
+ALTER TABLE `pedido_detalle`
+  ADD PRIMARY KEY (`idDetalle`),
+  ADD KEY `idPedido` (`idPedido`),
+  ADD KEY `idProducto` (`idProducto`);
+
+--
+-- Indices de la tabla `perfil`
+--
+ALTER TABLE `perfil`
+  ADD PRIMARY KEY (`idPerfil`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`idProducto`),
+  ADD KEY `idProducto` (`idProducto`),
+  ADD KEY `idCategoria` (`idCategoria`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`),
+  ADD KEY `idPerfil` (`idPerfil`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `formapago`
+--
+ALTER TABLE `formapago`
+  MODIFY `IdFormaPago` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `perfil`
+--
+ALTER TABLE `perfil`
+  MODIFY `idPerfil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD CONSTRAINT `factura_ibfk_1` FOREIGN KEY (`idPedido`) REFERENCES `pedido` (`idPedido`);
+
+--
+-- Filtros para la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`);
+
+--
+-- Filtros para la tabla `pedido_detalle`
+--
+ALTER TABLE `pedido_detalle`
+  ADD CONSTRAINT `pedido_detalle_ibfk_1` FOREIGN KEY (`idPedido`) REFERENCES `pedido` (`idPedido`),
+  ADD CONSTRAINT `pedido_detalle_ibfk_2` FOREIGN KEY (`idProducto`) REFERENCES `productos` (`idProducto`);
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`idPerfil`) REFERENCES `perfil` (`idPerfil`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
 
 --
 -- Indices de la tabla `pedido_detalle`
